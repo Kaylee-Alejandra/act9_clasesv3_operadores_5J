c15c2c5e3f181f60f09d33a96dcb93ae405ab78e
kaylee luevano molina <a.22308051281248@cbtis128.edu.mx>
2024-09-20 09:27:07 -0600

    Add files via upload

---
+++ operadoresA.py
@@ -0,0 +1,47 @@
+print("Act 9 clases v2")
+print("Kaylee Luevano Mat: 22308051281248")
+# zona de clases
+
+class Operadores1248:
+    #zona de funciones 
+    def suma1248(self, K, L):
+        s1248= K+L
+        print(f"La suma de {K} + {L} = {s1248}")
+    def resta1248(self, a,m):
+        r1248= a-m
+        print(f"La resta de {a} - {m} = {r1248}")
+    def multiplicacion1248(self, p,b):
+        m1248= p*b
+        print(f"La multiplicacion de {p} x {b} = {m1248}")
+    def division1248(self, d,h):
+        d1248= d/h
+        print(f"La division de {d} / {h} = {d1248}")
+    def modulo1248(self, r,s):
+        mo1248= r%s
+        print(f"El modulo de {r} % {s} = {mo1248}")
+    def exponente1248(self, e,x):
+        e1248= e**x
+        print(f"El exponente de {e} a la {x} = {e1248}")
+    def cociente1248(self, g,c):
+        c1248= g//c
+        print(f"El cociente de {g} // {c} = {c1248}")
+
+#Creacion de objetos 
+operadorLuevano= Operadores1248()
+
+
+# Zona de uso deobjetos 
+print("Funcion suma")
+operadorLuevano.suma1248(5,4)
+print("Funcion resta")
+operadorLuevano.resta1248(10,5)
+print("Funcion multiplicacion")
+operadorLuevano.multiplicacion1248(2,3)
+print("Funcion division")
+operadorLuevano.division1248(4,2)
+print("Funcion modulo")
+operadorLuevano.modulo1248(5,2)
+print("Funcion exponente")
+operadorLuevano.exponente1248(2,3)
+print("Funcion cociente")
+operadorLuevano.cociente1248(3,1)
